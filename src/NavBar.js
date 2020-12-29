e8df509eb096b04cdde70b2b27cb0d729a3655f8
Facundo Ottalagano <facuottalagano@gmail.com>
2020-12-28 22:11:49 -0300

    nuevo componente NavBar

---
+++ src/NavBar.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+
+
+const App = function(){
+  return(
+    <>     
+      <header>
+        <h1>Guitarras Lider</h1>
+      </header>
+      <p>Guitarras viejas</p>
+    </>
+  )
+}
+
+export default App;
